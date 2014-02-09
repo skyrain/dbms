e733705b4c49d26c30384c65fbeb65aa9406bea7
Tianyu Qiu <skyrain363@gmail.com>
2014-02-09 16:25:18 -0700

    1st

---
+++ proj1/HeapFile/src/scan.C
@@ -0,0 +1,34 @@
+/*
+ * implementation of class Scan for HeapFile project.
+ * $Id: scan.C,v 1.1 1997/01/02 12:46:42 flisakow Exp $
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "heapfile.h"
+#include "scan.h"
+#include "hfpage.h"
+#include "buf.h"
+#include "db.h"
+
+// *******************************************
+Scan::Scan (HeapFile *hf, Status& status)
+{
+  status = OK;
+}
+
+// *******************************************
+Scan::~Scan()
+{
+  // put your code here
+}
+
+// *******************************************
+// Retrieve the next record in a sequential scan.
+// Also returns the RID of the retrieved record.
+Status Scan::getNext(RID& rid, char *recPtr, int& recLen)
+{
+  // put your code here
+  return OK;
+}
