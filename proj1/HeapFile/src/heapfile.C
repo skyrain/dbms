ef2ec72ec6c880bfd4d9beb518787e390f6be9dc
Farmost East <octaw@sina.com>
2014-02-27 00:16:33 -0700

    Update heapfile.C

--- proj1/HeapFile/src/heapfile.C
+++ proj1/HeapFile/src/heapfile.C
@@ -1,3 +1,53 @@
+/*
+heapfile.C
+Define the heapfile and all the operations on a heapfile.
+
+- Overall description of your algorithms and data structures
+
+  HeapFile::HeapFile( const char *name, Status& returnStatus ): constructor of a heapfile, write into the disk if this is a new heapfile,
+  other wise, create a new heapfile and initialize all the members of a heapfile object.
+   
+  HeapFile::~HeapFile(): deconstructor of heapfile, delete the filename and call the deleteFile to delete the heap file.  
+
+  int HeapFile::getRecCnt(): it will return number of records in heap file by calling the getSlotCnt() by iterating all data pages.
+
+  Status HeapFile::insertRecord(char *recPtr, int recLen, RID& outRid): insert a record in the heapfile, return OK if insert successfully, return error code if not.
+  It will find a existing data page in the heapfile which has enough space for that record. If can not find any data page, then it will
+  create a new data page and insert the record into the new page.
+
+  Status HeapFile::deleteRecord (const RID& rid) delete a record in the heapfile, return OK if delete successfully, return error code if not. 
+  it will find out the record with given RID, then call deleteRecord to delete the corresponding record. Then determine whether the data page is empty, if it is empty,
+  that page will be deleted, otherwise just unpin that data page.
+  
+  Status HeapFile::updateRecord (const RID& rid, char *recPtr, int recLen) update a record in the heapfile, return OK if update successfully, return error code if not. 
+  find the data record with given Rid by calling findDataPage, if it is found, then update the data record and determine if the length of the data record has been changed.
+  if the data record is not found, just return DONE, this should be a error code (No problem since then)???
+  
+  Status HeapFile::getRecord (const RID& rid, char *recPtr, int& recLen) get a record in the heapfile, return OK if get the record successfully, return error code if not.  
+  find the data record with given Rid by calling findDataPage, if it is found, then get the data record and returning the record pointer and length.
+  
+  Scan *HeapFile::openScan(Status& status): Initiate a sequential scan and return it. 
+  
+  Status HeapFile::deleteFile(): Wipes out the heapfile from the database permanently, return OK if all the data page deleted successfully, otherwise return corresponding error code. 
+  
+  Status HeapFile::newDataPage(DataPageInfo *dpinfop) get a new data page from the buffer manager and initialize dpinfo.
+
+  Status HeapFile::findDataPage(const RID& rid, PageId &rpDirPageId, HFPage *&rpdirpage, PageId &rpDataPageId,HFPage *&rpdatapage): Return OK if the data page is found, otherwise return DONE.
+  returns pinned directory page and pinned data page of the specified user record If the user record cannot be found, rpdirpage and rpdatapage are returned as NULL pointers.
+					
+- Anything unusual in your implementation
+
+  
+  In implementation of heap file, I decide not to use the directory page structure, as a matter of fact that there is only a small number of data page in the heap file, also 
+  it's a single user database, one doubly linked list is more robust and easier to implemented, directory page will increase the speed of insert by checking the dpinfo, but the time which are saved is
+  insignificant and maintenaning the directory page also needs extra disk I/O and easy to corrupt.
+
+- What functionalities not supported well
+
+  All functionalities are fully supported.
+  
+*/
+
 #include "heapfile.h"   
 #include <stdio.h>   
 
@@ -164,10 +214,10 @@ Status HeapFile::deleteRecord (const RID& rid)
     PageId previoudPageId = INVALID_PAGE;
     PageId nextPageId = INVALID_PAGE;
     int freeSpace;	
-	
+    // if can allocate the page then set the flag to 1	
     int flag = 0;
 	
-    // find the data record with rid that required to be deleted
+    // find the data record with rid that required to be deleted thrn call deleteRecord
     Status status;
     while((flag != 1) && (pageId != INVALID_PAGE)){   
         MINIBASE_BM->pinPage(pageId,(Page*&)delPage);      
@@ -225,6 +275,7 @@ Status HeapFile::updateRecord (const RID& rid, char *recPtr, int recLen)
 	return MINIBASE_FIRST_ERROR(HEAPFILE, INVALID_UPDATE);
 
     Status status, found;
+    // call the findDataPage to find the data page with record rid.
     found = findDataPage(rid, dirPageId, dirPage, dataPageId, datapage);
     if(found == OK){ 
 	// if found the record, try to update it.
@@ -261,6 +312,7 @@ Status HeapFile::getRecord (const RID& rid, char *recPtr, int& recLen)
 
     Status status, found;
     
+    // call the findDataPage to find the data page with record rid.
     found = findDataPage(rid, dirPageId, dirPage, dataPageId, datapage);
     if(found == OK){ 
 	// if found, try to get the record
